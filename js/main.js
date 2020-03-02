0cabbb513aa23ab85e507064e271e5bc9274dcf8
osaroadade <adadechuks@gmail.com>
2020-03-02 15:28:11 +0100

    UI Fixes, Accessibility Fixes and Page Optimization.

--- js/main.js
+++ js/main.js
@@ -30,39 +30,39 @@
 -----------------------------------*/
 
 
-(function($) {
-    'use strict';
+(function ($) {
+  'use strict';
 
 
-/*-------------------------------------------
-  01. jQuery MeanMenu
---------------------------------------------- */
-    
-$('.mobile-menu nav').meanmenu({
+  /*-------------------------------------------
+    01. jQuery MeanMenu
+  --------------------------------------------- */
+
+  $('.mobile-menu nav').meanmenu({
     meanMenuContainer: '.mobile-menu-area',
     meanScreenWidth: "991",
     meanRevealPosition: "right",
-});
+  });
 
-/*-------------------------------------------
-  02. wow js active
---------------------------------------------- */
+  /*-------------------------------------------
+    02. wow js active
+  --------------------------------------------- */
 
   new WOW().init();
 
 
 
-/*-------------------------------------------
-  03. Product  Masonry (width)
---------------------------------------------- */ 
+  /*-------------------------------------------
+    03. Product  Masonry (width)
+  --------------------------------------------- */
 
-$('.htc__product__container').imagesLoaded( function() {
-  
+  $('.htc__product__container').imagesLoaded(function () {
+
     // filter items on button click
-    $('.product__menu').on( 'click', 'button', function() {
+    $('.product__menu').on('click', 'button', function () {
       var filterValue = $(this).attr('data-filter');
       $grid.isotope({ filter: filterValue });
-    }); 
+    });
     // init Isotope
     var $grid = $('.product__list').isotope({
       itemSelector: '.single__pro',
@@ -75,28 +75,28 @@ $('.htc__product__container').imagesLoaded( function() {
       }
     });
 
-});
+  });
 
-$('.product__menu button').on('click', function(event) {
+  $('.product__menu button').on('click', function (event) {
     $(this).siblings('.is-checked').removeClass('is-checked');
     $(this).addClass('is-checked');
     event.preventDefault();
-});
+  });
 
 
 
-/*-------------------------------------------
-  04. Sticky Header
---------------------------------------------- */ 
+  /*-------------------------------------------
+    04. Sticky Header
+  --------------------------------------------- */
 
 
   var win = $(window);
   var sticky_id = $("#sticky-header-with-topbar");
-  win.on('scroll',function() {    
+  win.on('scroll', function () {
     var scroll = win.scrollTop();
     if (scroll < 245) {
       sticky_id.removeClass("scroll-header");
-    }else{
+    } else {
       sticky_id.addClass("scroll-header");
     }
   });
@@ -105,137 +105,103 @@ $('.product__menu button').on('click', function(event) {
 
 
 
-/*--------------------------
-  05. ScrollUp
----------------------------- */
-$.scrollUp({
+  /*--------------------------
+    05. ScrollUp
+  ---------------------------- */
+  $.scrollUp({
     scrollText: '<i class="zmdi zmdi-chevron-up"></i>',
     easingType: 'linear',
     scrollSpeed: 900,
     animation: 'fade'
-});
+  });
 
 
 
-/*---------------------------
-  06. Tooltip
-------------------------------*/    
-$('[data-toggle="tooltip"]').tooltip({
+  /*---------------------------
+    06. Tooltip
+  ------------------------------*/
+  $('[data-toggle="tooltip"]').tooltip({
     animated: 'fade',
     placement: 'top',
     container: 'body'
-});
-
-
-
-/*-----------------------------------
-  07. ScrollReveal Js Init
--------------------------------------- */
-
-  window.sr = ScrollReveal({ duration: 800 , reset: false });
-    sr.reveal('.foo');
-    sr.reveal('.bar');
-
-
-
-/*-------------------------------------------------------
-  08. Fixed Footer bottom script ( Newsletter )
---------------------------------------------------------*/
-
-var $newsletter_height = $(".htc__foooter__area");
-$('.fixed__footer').css({'margin-bottom': $newsletter_height.height() + 'px'});
-
-
-
-
-/*------------------------------------    
-  09. Search Bar
---------------------------------------*/ 
-    
-  $( '.search__open' ).on( 'click', function () {
-    $( 'body' ).toggleClass( 'search__box__show__hide' );
-    return false;
-  });
-
-  $( '.search__close__btn .search__close__btn_icon' ).on( 'click', function () {
-    $( 'body' ).toggleClass( 'search__box__show__hide' );
-    return false;
   });
 
 
 
+  /*-----------------------------------
+    07. ScrollReveal Js Init
+  -------------------------------------- */
 
-/*------------------------------------    
-  10. Toogle Menu
---------------------------------------*/
+  window.sr = ScrollReveal({ duration: 800, reset: true });
+  sr.reveal('.foo');
+  sr.reveal('.bar');
 
-  $('.toggle__menu').on('click', function() {
+
+
+  /*-------------------------------------------------------
+    08. Fixed Footer bottom script ( Newsletter )
+  --------------------------------------------------------*/
+
+  var $newsletter_height = $(".htc__foooter__area");
+  $('.fixed__footer').css({ 'margin-bottom': $newsletter_height.height() + 'px' });
+
+
+
+
+  /*------------------------------------    
+    10. Toogle Menu
+  --------------------------------------*/
+
+  $('.toggle__menu').on('click', function () {
     $('.offsetmenu').addClass('offsetmenu__on');
     $('.body__overlay').addClass('is-visible');
 
   });
 
-  $('.offsetmenu__close__btn').on('click', function() {
-      $('.offsetmenu').removeClass('offsetmenu__on');
-      $('.body__overlay').removeClass('is-visible');
+  $('.offsetmenu__close__btn').on('click', function () {
+    $('.offsetmenu').removeClass('offsetmenu__on');
+    $('.body__overlay').removeClass('is-visible');
   });
 
 
+  /*------------------------------------    
+    12. Filter Area
+  --------------------------------------*/
 
-/*------------------------------------    
-  11. Shopping Cart Area
---------------------------------------*/
-
-  $('.cart__menu').on('click', function() {
-    $('.shopping__cart').addClass('shopping__cart__on');
-    $('.body__overlay').addClass('is-visible');
-
-  });
-
-  $('.offsetmenu__close__btn').on('click', function() {
-      $('.shopping__cart').removeClass('shopping__cart__on');
-      $('.body__overlay').removeClass('is-visible');
-  });
-
-
-/*------------------------------------    
-  12. Filter Area
---------------------------------------*/
-
-  $('.filter__menu').on('click', function() {
+  $('.filter__menu').on('click', function () {
     $('.filter__wrap').addClass('filter__menu__on');
     $('.body__overlay').addClass('is-visible');
 
   });
 
-  $('.filter__menu__close__btn').on('click', function() {
-      $('.filter__wrap').removeClass('filter__menu__on');
-      $('.body__overlay').removeClass('is-visible');
+  $('.filter__menu__close__btn').on('click', function () {
+    $('.filter__wrap').removeClass('filter__menu__on');
+    $('.body__overlay').removeClass('is-visible');
   });
 
 
 
-/*------------------------------------    
-  13. User Menu
---------------------------------------*/
+  /*------------------------------------    
+    13. User Menu
+  --------------------------------------*/
 
-  $('.user__menu').on('click', function() {
+  $('.user__menu').on('click', function () {
     $('.user__meta').addClass('user__meta__on');
     $('.body__overlay').addClass('is-visible');
 
   });
 
-  $('.offsetmenu__close__btn').on('click', function() {
-      $('.user__meta').removeClass('user__meta__on');
-      $('.body__overlay').removeClass('is-visible');
+  $('.offsetmenu__close__btn').on('click', function () {
+    $('.user__meta').removeClass('user__meta__on');
+    $('.body__overlay').removeClass('is-visible');
   });
 
 
 
-/*------------------------------------    
-  14. Overlay Close
---------------------------------------*/
-  $('.body__overlay').on('click', function() {
+  /*------------------------------------    
+    14. Overlay Close
+  --------------------------------------*/
+  $('.body__overlay').on('click', function () {
     $(this).removeClass('is-visible');
     $('.offsetmenu').removeClass('offsetmenu__on');
     $('.shopping__cart').removeClass('shopping__cart__on');
@@ -245,27 +211,27 @@ $('.fixed__footer').css({'margin-bottom': $newsletter_height.height() + 'px'});
 
 
 
-/*-----------------------------------------------
-  15. Home Slider
--------------------------------------------------*/
+  /*-----------------------------------------------
+    15. Home Slider
+  -------------------------------------------------*/
 
   if ($('.slider__activation__wrap').length) {
     $('.slider__activation__wrap').owlCarousel({
       loop: true,
-      margin:0,
-      nav:true,
+      margin: 0,
+      nav: true,
       autoplay: false,
-      navText: [ '<i class="zmdi zmdi-chevron-left"></i>', '<i class="zmdi zmdi-chevron-right"></i>' ],
+      navText: ['<i class="zmdi zmdi-chevron-left"></i>', '<i class="zmdi zmdi-chevron-right"></i>'],
       autoplayTimeout: 10000,
-      items:1,
+      items: 1,
       dots: false,
       lazyLoad: true,
-      responsive:{
-        0:{
-          items:1
+      responsive: {
+        0: {
+          items: 1
         },
-        1920:{
-          items:1
+        1920: {
+          items: 1
         }
       }
     });
@@ -274,19 +240,19 @@ $('.fixed__footer').css({'margin-bottom': $newsletter_height.height() + 'px'});
   if ($('.slider__activation__02').length) {
     $('.slider__activation__02').owlCarousel({
       loop: true,
-      margin:0,
-      nav:false,
+      margin: 0,
+      nav: false,
       autoplay: false,
       autoplayTimeout: 10000,
-      items:1,
+      items: 1,
       dots: false,
       lazyLoad: true,
-      responsive:{
-        0:{
-          items:1
+      responsive: {
+        0: {
+          items: 1
         },
-        1920:{
-          items:1
+        1920: {
+          items: 1
         }
       }
     });
@@ -295,166 +261,166 @@ $('.fixed__footer').css({'margin-bottom': $newsletter_height.height() + 'px'});
 
 
 
-/*-----------------------------------------------
-  16. Popular Product Wrap
--------------------------------------------------*/
+  /*-----------------------------------------------
+    16. Popular Product Wrap
+  -------------------------------------------------*/
 
 
   $('.popular__product__wrap').owlCarousel({
-      loop: true,
-      margin:0,
-      nav:true,
-      autoplay: false,
-      navText: [ '<i class="zmdi zmdi-chevron-left"></i>', '<i class="zmdi zmdi-chevron-right"></i>' ],
-      autoplayTimeout: 10000,
-      items:3,
-      dots: false,
-      lazyLoad: true,
-      responsive:{
-        0:{
-          items:1
-        },
-        600:{
-          items:2
-        },
-        800:{
-          items:2
-        },
-        1024:{
-          items:3
-        },
-        1200:{
-          items:3
-        },
-        1400:{
-          items:3
-        },
-        1920:{
-          items:3
-        }
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoplay: false,
+    navText: ['<i class="zmdi zmdi-chevron-left"></i>', '<i class="zmdi zmdi-chevron-right"></i>'],
+    autoplayTimeout: 10000,
+    items: 3,
+    dots: false,
+    lazyLoad: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      800: {
+        items: 2
+      },
+      1024: {
+        items: 3
+      },
+      1200: {
+        items: 3
+      },
+      1400: {
+        items: 3
+      },
+      1920: {
+        items: 3
       }
-    });
+    }
+  });
 
 
 
-/*-----------------------------------------------
-  17. Testimonial Wrap
--------------------------------------------------*/
+  /*-----------------------------------------------
+    17. Testimonial Wrap
+  -------------------------------------------------*/
 
 
   $('.testimonial__wrap').owlCarousel({
-      loop: true,
-      margin:0,
-      nav:false,
-      autoplay: false,
-      navText: false,
-      autoplayTimeout: 10000,
-      items:1,
-      dots: false,
-      lazyLoad: true,
-      responsive:{
-        0:{
-          items:1
-        },
-        600:{
-          items:1
-        },
-        800:{
-          items:1
-        },
-        1024:{
-          items:1
-        },
-        1200:{
-          items:1
-        },
-        1400:{
-          items:1
-        },
-        1920:{
-          items:1
-        }
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay: false,
+    navText: false,
+    autoplayTimeout: 10000,
+    items: 1,
+    dots: false,
+    lazyLoad: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      800: {
+        items: 1
+      },
+      1024: {
+        items: 1
+      },
+      1200: {
+        items: 1
+      },
+      1400: {
+        items: 1
+      },
+      1920: {
+        items: 1
       }
-    });
+    }
+  });
 
 
 
 
-/*--------------------------------
-  18. Magnific Popup
-----------------------------------*/
+  /*--------------------------------
+    18. Magnific Popup
+  ----------------------------------*/
 
-$('.video-popup').magnificPopup({
-  type: 'iframe',
-  mainClass: 'mfp-fade',
-  removalDelay: 160,
-  preloader: false,
-  zoom: {
+  $('.video-popup').magnificPopup({
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    zoom: {
       enabled: true,
-  }
-});
+    }
+  });
 
-$('.image-popup').magnificPopup({
-  type: 'image',
-  mainClass: 'mfp-fade',
-  removalDelay: 100,
-  gallery:{
-      enabled:true, 
-  }
-});
-
-
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    mainClass: 'mfp-fade',
+    removalDelay: 100,
+    gallery: {
+      enabled: true,
+    }
+  });
 
 
 
-/*-------------------------------
-  19. Price Slider Active
---------------------------------*/
+
+
+  /*-------------------------------
+    19. Price Slider Active
+  --------------------------------*/
   $("#slider-range").slider({
-      range: true,
-      min: 10,
-      max: 500,
-      values: [110, 400],
-      slide: function(event, ui) {
-          $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-      }
+    range: true,
+    min: 10,
+    max: 500,
+    values: [110, 400],
+    slide: function (event, ui) {
+      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+    }
   });
   $("#amount").val("$" + $("#slider-range").slider("values", 0) +
-      " - $" + $("#slider-range").slider("values", 1));
+    " - $" + $("#slider-range").slider("values", 1));
 
 
 
 
-/*-------------------------------
-  20.  Plus Minus Button 
---------------------------------*/
+  /*-------------------------------
+    20.  Plus Minus Button 
+  --------------------------------*/
 
 
-    $(".cart-plus-minus").append('<div class="dec qtybutton">-</i></div><div class="inc qtybutton">+</div>');
+  $(".cart-plus-minus").append('<div class="dec qtybutton">-</i></div><div class="inc qtybutton">+</div>');
 
-    $(".qtybutton").on("click", function () {
-        var $button = $(this);
-        var oldValue = $button.parent().find("input").val();
-        if ($button.text() == "+") {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            // Don't allow decrementing below zero
-            if (oldValue > 1) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 1;
-            }
-        }
-        $button.parent().find("input").val(newVal);
-    });
+  $(".qtybutton").on("click", function () {
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();
+    if ($button.text() == "+") {
+      var newVal = parseFloat(oldValue) + 1;
+    } else {
+      // Don't allow decrementing below zero
+      if (oldValue > 1) {
+        var newVal = parseFloat(oldValue) - 1;
+      } else {
+        newVal = 1;
+      }
+    }
+    $button.parent().find("input").val(newVal);
+  });
 
 
-/*--------------------------
-  21. jQuery scroll Nav
----------------------------- */
-    $('.onepage--menu').onePageNav({
-        scrollOffset: 0
-    }); 
+  /*--------------------------
+    21. jQuery scroll Nav
+  ---------------------------- */
+  $('.onepage--menu').onePageNav({
+    scrollOffset: 0
+  });
 
 
 
@@ -466,23 +432,23 @@ $('.image-popup').magnificPopup({
 })(jQuery);
 
 // Placeholder and Label Animation
-$('input, textarea').focus(function(){
+$('input, textarea').focus(function () {
   $(this).parents('.form--group').addClass('focused');
 });
 
 
-$('input, textarea').blur(function(){
+$('input, textarea').blur(function () {
   var inputValue = $(this).val();
-  if ( inputValue == "" ) {
+  if (inputValue == "") {
     $(this).removeClass('filled');
-    $(this).parents('.form--group').removeClass('focused');  
+    $(this).parents('.form--group').removeClass('focused');
   } else {
     $(this).addClass('filled');
   }
 })
 
 //Show Hidden Password
-$(".toggle--password").click(function() {
+$(".toggle--password").click(function () {
 
   $(this).toggleClass("zmdi-eye zmdi-eye-off");
   var input = $($(this).attr("toggle"));
@@ -494,15 +460,19 @@ $(".toggle--password").click(function() {
 });
 
 //Show update status for merchant page
-$(".commit--changes__personal").click(function() {
+$(".commit--changes__personal").click(function () {
 
   $("#change--effected__account").toggleClass("hide view");
 });
-$(".commit--changes__business").click(function() {
+$(".commit--changes__business").click(function () {
 
   $("#change--effected__business").toggleClass("hide view");
 });
-$(".commit--changes__login").click(function() {
+$(".commit--changes__login").click(function () {
 
   $("#change--effected__login").toggleClass("hide view");
+});
+$(".commit--changes__delete").click(function () {
+
+  $("#change--effected__delete").toggleClass("hide view");
 });
