2f9ffc2cf8afc25e35ae3141895b1f25d940c995
osaroadade <adadechuks@gmail.com>
2020-03-03 08:13:59 +0100

    Toast Notification Fix

--- js/main.js
+++ js/main.js
@@ -485,4 +485,10 @@ $(document).ready(function() {
   $('a.exit--toast').click(function() {
       $("#openToast").fadeOut(1000, 0);
   });
+
+  $(document).on('click', function (e) {
+    if ($(e.target).closest(".notification--reminder").length === 0) {
+        $("#openToast").fadeOut(1000, 0);
+    }
+});
 });
